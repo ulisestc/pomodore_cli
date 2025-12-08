3ca0edcd5f433c004ee708e2993060a51fb9e317
ulisestc <ulisestc27@gmail.com>
2025-12-07 20:54:02 -0600

    Finished implementation of initial configuration ( still have to fix handling string user input

---
+++ src/app.py
@@ -0,0 +1,84 @@
+import time, os
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+
+    config = {
+        "POMODORE_TIME" : 25,
+        "SHORT_REST_TIME" : 5,
+        "LONG_REST_TIME" : 15,
+        "NUMBER_OF_ROUNDS" : 4
+    }
+    
+    clear_terminal()
+
+    is_default = "Y"
+    is_default = input("""
+                        Welcome to pomodore CLI, do you want to use default settings?
+                        (Y/n)
+                        """)
+    
+    if is_default == "": is_default = "Y" # si no se elige nada => Y, como si fuera linux
+    clear_terminal()
+    # DEBUG
+    # print(f"is_default:{is_default}")
+
+    if is_default == "n":
+
+        options = ["Focus time", "Short rest time", "Long rest time", "Number of rounds"]
+        
+        while True:
+            option_to_modify = int(input(f"""
+                                        What timer do you want to modify?
+                                        1. Focus time ({config["POMODORE_TIME"]} mins)
+                                        2. Short rest ({config["SHORT_REST_TIME"]} mins)
+                                        3. Long Rest ({config["LONG_REST_TIME"]} mins)
+                                        4. Number of rounds ({config["NUMBER_OF_ROUNDS"]})
+                                        5. Exit
+                                    """))
+            if option_to_modify > 5 or option_to_modify < 1:
+                clear_terminal()
+        
+            if option_to_modify == 5:
+                # exit = True
+                break
+
+            ans = int(input(f"insert the new value for {options[option_to_modify-1]}:"))
+            
+            if option_to_modify == 1:
+                config["POMODORE_TIME"] = ans
+            elif option_to_modify == 2:
+                config["SHORT_REST_TIME"] = ans
+            elif option_to_modify == 3:
+                config["LONG_REST_TIME"] = ans
+            elif option_to_modify == 4:
+                config["NUMBER_OF_ROUNDS"] = ans
+
+            clear_terminal()
+            
+    print(f"""Using values:
+        FOCUS time: {config["POMODORE_TIME"]} mins
+        REST time: {config["SHORT_REST_TIME"]} mins
+        LONG_REST time: {config["LONG_REST_TIME"]} mins
+        nuber of ROUNDS: {config["NUMBER_OF_ROUNDS"]}
+    """)
+
+if __name__ == '__main__':
+    main()
+
+
+# SOURCES:
+#  ESTE PROYECTO TIENE COMO PRINCIPAL OBJETIVO EL AFIANZAR MIS CONOCIMIENTOS Y LÓGICA DE PROGRAMACIÓN MEDIANTE
+#  LA INVESTIGACIÓN PROPIA DE MANERA PRAGMÁTICA, PARA MEDIR CUANTITATIVAMENTE ANOTARÉ TODA FUENTE DE CONSULTA 
+#     26/11/2025
+#     https://www.programiz.com/python-programming/main-function
+#     https://www.geeksforgeeks.org/python/python-or-operator/ 
+#     https://www.w3schools.com/python/ref_string_isnumeric.asp   
+#     https://stackoverflow.com/questions/2084508/clear-the-terminal-in-python
+
+#     07/12/2025
+#     https://stackoverflow.com/questions/74412503/cannot-access-local-variable-a-where-it-is-not-associated-with-a-value-but
+#     https://search.brave.com/search?q=python+input+to+int&summary=1&conversation=b1457da7883e657cf43a76
+
