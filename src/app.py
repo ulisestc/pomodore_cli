c55a33b02c92ce6f8aa4d06de69e13c15bd7a9f4
ulisestc <ulisestc27@gmail.com>
2026-01-27 10:41:40 -0600

    Se empezó a idear la TUI con textual

--- src/app.py
+++ src/app.py
@@ -1,8 +1,34 @@
 from textual.app import App
+from textual.widgets import Header, Footer, Static, Button
+from textual.containers import Container
 
-class MyApp(App):
-    pass
+class MainMenu(Static):
+    def compose(self):
+        yield Static("POMODORE TUI", id="title")
+        yield Button("START SESSION")
+        yield Button("SETTINGS")
+
+class PomodoreTUI(App):
+    CSS_PATH = "styles.css"
+
+    BINDINGS = [
+        ("s","start_session", "Start Session"),
+        ("t","go_to_settings", "Go to Settings")
+    ]
+
+    def compose(self):
+        yield Header()
+        yield Footer()
+
+        with Container(id ="menu"):
+            yield MainMenu()
+
+    def action_start_session(self):
+        pass
+
+    def action_go_to_settings(self):
+        pass
 
 if __name__ == "__main__":
-    app = MyApp()
+    app = PomodoreTUI()
     app.run()
